f604efe652513213e20d5626c64e0476adab6a2e
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2022-03-14 07:14:29 +0530

    deep copy function

--- Copying/object.js
+++ Copying/object.js
@@ -123,3 +123,33 @@ console.log(person1);
 //       country: 'USA'
 //     }
 //   }
+
+
+// 3
+var personalDetail = {
+	name : 'Nishant',
+	address : {
+	  location: 'xyz',
+	  zip : '123456',
+	  phoneNumber : {
+	    homePhone: 8797912345,
+	    workPhone : 1234509876
+	  }
+	}
+}
+
+function deepClone(object){
+	var newObject = {};
+	for(var key in object){
+		if(typeof object[key] === 'object'  && object[key] !== null ){
+		 newObject[key] = deepClone(object[key]);
+		}else{
+		 newObject[key] = object[key];
+		}
+	}
+	return newObject;
+}
+
+var newObject = deepClone(personalDetail);
+console.log(newObject)
+
