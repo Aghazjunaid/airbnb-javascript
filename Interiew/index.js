8f316d9817b57ac95f8502cf381a41989df659df
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2024-08-11 13:13:36 +0530

    Update index.js

--- Interiew/index.js
+++ Interiew/index.js
@@ -53,4 +53,23 @@ for Post
     }
 ]
 
-find pivot in rotated sorted Array
+//find pivot in rotated sorted Array
+
+var calc = {
+    total: 0,
+    add(a) {
+        this.total += a;
+        return this;
+    },
+    subtract(a) {
+        this.total -= a;
+        return this;
+    },
+    multiply(a) {
+        this.total *= a;
+        return this;
+    },
+};
+let result = calc.add(10).multiply(5).subtract(30).add(10) 
+console.log(result.total);
+
