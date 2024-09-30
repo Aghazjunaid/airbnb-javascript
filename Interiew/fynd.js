a27f2068e169b15046bc1f6314b8a3c8e7198a41
Aghaz Junaid <aghaz.dev@gmail.com>
2024-09-30 21:38:42 +0530

    misc upd

--- Interiew/fynd.js
+++ Interiew/fynd.js
@@ -14,7 +14,7 @@ items.push(6);
 
 const url = `api.facebook.com/posts/:postId`
 
-Write a function which accepts postId as arg and makes a GET request with a postId return the post data.
+//Write a function which accepts postId as arg and makes a GET request with a postId return the post data.
 
 {
 	_id: 5,
@@ -78,19 +78,6 @@ console.log("100");
 await sleep(2000);
 console.log("200");
 
-//solution
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function demo() {
-  console.log('Taking a break...');
-  await sleep(2000);
-  console.log('Two second break is over');
-}
-
-demo();
-
 useEffect(()=>{
 	
   return (()=>{
@@ -99,87 +86,4 @@ useEffect(()=>{
 },[])
 
 
-//Sure, here's a simple example of how to create a circle using HTML and CSS:
-<div class="circle"></div>
-.circle {
-  width: 100px;
-  height: 100px;
-  background-color: #555;
-  border-radius: 50%;
-}
-
-//
-const express = require('express');
-const app = express();
-
-
-const mid1 = function(req,res,next){
-    console.log('mid 1 running');
-    next()
-}
-
-const ErrorHandler = (err, req, res, next) => {
-    console.log(err,next);
-
-    console.log("Middleware Error Hadnling");
-    const errStatus = err.statusCode || 500;
-    const errMsg = err.message || 'Something went wrong';
-    res.status(errStatus).json({
-        success: false,
-        status: errStatus,
-        message: errMsg,
-        stack: process.env.NODE_ENV === 'development' ? err.stack : {}
-    })
-}
-
-app.use(ErrorHandler)
-
-
-app.get('/',(req,res,next) => {
-    try{
-        throw "aj"
-    } catch(err){
-        next(err)
-
-    }
-    // return res.json("hello")
-})
-
-
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-app.get('/aj',(req,res,next) => {
-    console.log('mid1', req.url);
-    req.mydata = 'Hello world';
-    const error = new Error('Something went wrong');
-    next(error);
-})
-
-
-
-app.listen(1337,()=>{
-    console.log('3000 port')
-})
-
-//========
-app.get('/', (req, res, next) => {
-    try {
-        // Some code that might throw an error
-        const data = someFunctionThatMightThrowError();
-        res.status(200).json(
-            { message: 'Data retrieved successfully', data });
-    } catch (error) {
-        next(error);
-    }
-});
-
-// Custom error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json(
-        { message: 'Something went wrong!' });
-});
 
