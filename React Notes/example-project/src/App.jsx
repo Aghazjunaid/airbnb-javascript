4778e0d2b5582ff16c189bccf1d4098d77ef4a7e
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2024-10-01 14:59:34 +0530

    Update App.jsx

--- React Notes/example-project/src/App.jsx
+++ React Notes/example-project/src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+import ButtonComponent from './Component/Button';
+import UseRef from './Component/UseRef';
+import Stopwatch from './Component/Stopwatch';
+import Debounce from './Component/Debounce';
+import UndoRedo from './Component/UndoRedo';
+import NestedComponent from './Component/NestedComments';
+import Todo from './Component/Todo';
+import Pagination from './Component/Pagination';
+import OTPPage from './Component/OtpVerify';
+import ChatGpt from './Component/ChatGpt';
+import CodeEditorComponent from './Component/CodeEditor';
+import LargeList from './Pages/LargeList';
+import { ThemeProvider } from './Component/Context/ThemeContext';
+import ToggleTheme from './Component/Context/TogglePage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [form, setForm] = useState({name: '', age: ''})
+  // const [data, setData] = useState([])
+  // function submitBtn(){
+  //   console.log(form)
+  // }
+
+  // function getData(){
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //   .then((res)=> setData(res.data))
+  //   .catch((err)=> console.log(err))
+  // }
+
+  // console.log(data)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider>
+      {/* <ButtonComponent/> */}
+      {/* <UseRef/> */}
+      {/* <Stopwatch/> */}
+      {/* <Debounce/> */}
+      {/* <UndoRedo/> */}
+{/* <NestedComponent/> */}
+{/* <Todo/> */}
+{/* <Pagination/> */}
+{/* <OTPPage/> */}
+{/* <ChatGpt/> */}
+{/* <CodeEditorComponent /> */}
+{/* <LargeList/> */}
+<ToggleTheme />
+      {/* <input type="text" name='name' onChange={(e)=> setForm({...form,name:e.target.value})}/>
+      <input type="text" name='age' onChange={(e)=> setForm({...form,age:e.target.value})}/>
+      <button onClick={submitBtn}>Click me</button>
+      <button onClick={getData}>Show data</button>
+      {data.length > 0 ? data.map((ele) => {
+        return (<>
+          <h2>{ele.id}</h2>
+          <p>{ele.title}</p>
+        </>)
+      }) : <h2>No data</h2>} */}
+    </ThemeProvider>
   )
 }
 
