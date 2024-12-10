b9288dae3eede374e591fb23df3032ac6792fb31
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2024-12-10 18:32:01 +0530

    Create landmark.js

---
+++ Interiew/landmark.js
@@ -0,0 +1,127 @@
+//1
+var map = new Map();
+map.set('orange', 10);
+map.set('apple', 5);
+map.set('banana', 20);
+map.set('cherry', 13);
+
+// console.log(map)
+
+const sortedMap = new Map([...map].sort((a,b) => a[1]-b[1]))
+
+console.log(sortedMap)
+
+//2
+const str = "abc";
+
+function perm(str){
+  const results = [];
+  
+  const permute = (prefix, rem) => {
+    if(rem.length === 0){
+      results.push(prefix)
+    }
+    
+    for(let i=0;i<rem.length;i++){
+      permute(prefix + rem[i], rem.slice(0,i) + rem.slice(i+1))
+    }
+  }
+  
+  permute('',str);
+  return results
+}
+
+console.log(perm(str))
+
+//3
+-- create
+CREATE TABLE EMPLOYEE (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  dept TEXT NOT NULL
+);
+
+-- create
+CREATE TABLE ADDRESS (
+  id INTEGER PRIMARY KEY,
+  address TEXT NOT NULL,
+  city TEXT NOT NULL,
+  empId INTEGER  NOT NULL
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Clark', 'Sales');
+INSERT INTO EMPLOYEE VALUES (0002, 'Dave', 'Accounting');
+INSERT INTO EMPLOYEE VALUES (0003, 'Ava', 'Sales');
+
+INSERT INTO EMPLOYEE VALUES (0004, 'Atul', 'Sales');
+
+INSERT INTO ADDRESS VALUES (0001, 'address', 'city', 0002);
+INSERT INTO ADDRESS VALUES (0002, 'address2', 'city2', 0002);
+
+INSERT INTO ADDRESS VALUES (0003, 'address4', 'city4', 0001);
+
+INSERT INTO ADDRESS VALUES (0004, 'address3', 'city3', 0003);
+
+-- fetch 
+SELECT * FROM EMPLOYEE ;
+
+select * from ADDRESS ;
+
+
+select count(e.empId) as empCount, e.name from EMPLOYEE e
+inner join ADDRESS a on a.empId = e.empId
+group by e.empId having count(e.empId) > 1;
+
+//4
+db={
+  "orders": [
+    {
+      "_id": 1,
+      "item": "almonds",
+      "price": 12,
+      "quantity": 2
+    },
+    {
+      "_id": 2,
+      "item": "pecans",
+      "price": 20,
+      "quantity": 1
+    },
+    {
+      "_id": 3
+    }
+  ],
+  "inventory": [
+    {
+      "_id": 1,
+      "sku": "almonds",
+      "description": "product 1",
+      "instock": 120
+    },
+    {
+      "_id": 2,
+      "sku": "bread",
+      "description": "product 2",
+      "instock": 80
+    },
+    {
+      "_id": 3,
+      "sku": "cashews",
+      "description": "product 3",
+      "instock": 60
+    },
+    {
+      "_id": 4,
+      "sku": "pecans",
+      "description": "product 4",
+      "instock": 70
+    },
+    {
+      "_id": 5,
+      "sku": null,
+      "description": "Incomplete"
+    }
+  ]
+}
+write join in this
